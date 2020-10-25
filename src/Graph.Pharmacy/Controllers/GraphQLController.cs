495407487433f44125545a80ed9367b30d06c815
Assil Abdulrahim <assilabdulrahim@live.com>
2020-10-25 08:03:03 -0400

    Added basics

---
+++ src/Graph.Pharmacy/Controllers/GraphQLController.cs
@@ -0,0 +1,57 @@
+﻿namespace Graph.Pharmacy.Controllers
+{
+    using Graph.Pharmacy.PharmaGraph;
+    using GraphQL;
+    using GraphQL.Instrumentation;
+    using GraphQL.Types;
+    using GraphQL.Validation.Complexity;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphQLController : ControllerBase
+    {
+        public async Task<IActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            System.DateTime start = DateTime.UtcNow;
+
+            //var inputs = query.Variables.Value;
+            var schema = new Schema();
+            //if (query.QueryType == "droidQuery")
+            //{
+            //    schema.Query = new DroidQuery();
+            //}
+            //else
+            //{
+            //    schema.Query = new WeatherForcastQuery();
+            //}
+
+            var result = await new DocumentExecuter().ExecuteAsync(executionOptions =>
+            {
+                executionOptions.Schema = schema;
+                executionOptions.Query = query.Query;
+                executionOptions.OperationName = query.OperationName;
+                executionOptions.Inputs = null;
+                executionOptions.EnableMetrics = true;
+                executionOptions.FieldMiddleware.Use<InstrumentFieldsMiddleware>();
+                //executionOptions.FieldMiddleware.Use<LoggingMiddleware>();
+                executionOptions.ComplexityConfiguration = new ComplexityConfiguration { MaxDepth = 15 };
+                //executionOptions.ValidationRules = new[]
+                //                                {
+                //                                  new RequiresAuthValidationRule()
+                //                                }
+                //                                .Concat(DocumentValidator.CoreRules);
+            }).ConfigureAwait(false);
+            result.EnrichWithApolloTracing(start);
+
+            if (result.Errors?.Count > 0)
+            {
+                return BadRequest(result.Errors[0].InnerException?.ToString() ?? result.Errors[0].Message);
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Graph.Pharmacy/PharmaGraph/GraphQLQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace Graph.Pharmacy.PharmaGraph
+{
+    using Newtonsoft.Json.Linq;
+
+    public class GraphQLQuery
+    {
+        public string NamedQuery { get; set; }
+        public string OperationName { get; set; }
+        public string Query { get; set; }
+        public string QueryType { get; set; }
+        public JObject Variables { get; set; }
+    }
+}
