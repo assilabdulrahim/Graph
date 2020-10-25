271b5340b1ddfe01d108ba465782d4fa422dfdad
Assil Abdulrahim <assilabdulrahim@live.com>
2020-10-25 11:35:44 -0400

    Add mutation to add a new patient

--- src/Graph.Pharmacy/PharmaGraph/Mutations/PatientMutation.cs
+++ src/Graph.Pharmacy/PharmaGraph/Mutations/PatientMutation.cs
@@ -1,29 +1,31 @@
 ﻿namespace Graph.Pharmacy.PharmaGraph.Mutations
 {
     using Graph.Pharmacy.Models;
+    using Graph.Pharmacy.PharmaGraph.Schemas;
+    using GraphQL;
     using GraphQL.Types;
-    using System;
 
     public class PatientMutation : ObjectGraphType
     {
         public PatientMutation()
         {
-            //Field<Patient>(
-            //        "mutatePatient",
-            //        arguments: new QueryArguments(
-            //          new QueryArgument<Patient> { }
-            //        ),
-            //        resolve: context =>
-            //        {
-            //            var newPatient = context.GetArgument<PatientModel>("patient");
-            //            return AddPatient(newPatient);
-            //        }
-            //      );
+            Field<Patient>(
+                    "AddPatient",
+                    arguments: new QueryArguments(
+                      new QueryArgument<Patient> { Name = "patient" }
+                    ),
+                    resolve: context =>
+                    {
+                        var newPatient = context.GetArgument<PatientModel>("patient");
+                        return AddPatient(newPatient);
+                    }
+                  );
         }
 
         private PatientModel AddPatient(PatientModel newPatient)
         {
-            throw new NotImplementedException();
+            //it better returns all Query to the new resource.
+            return newPatient;
         }
     }
 }
